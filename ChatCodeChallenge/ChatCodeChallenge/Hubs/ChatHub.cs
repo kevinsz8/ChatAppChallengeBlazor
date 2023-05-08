2137ab4dab40c1d9fec698f269fc50b3bf7d435a
kevinsz8 <kevinsz2805@outlook.com>
2023-05-07 19:54:46 -0600

    Adding call to make the difference if is a normal message or this is a "/stock=" command message

--- ChatCodeChallenge/ChatCodeChallenge/Hubs/ChatHub.cs
+++ ChatCodeChallenge/ChatCodeChallenge/Hubs/ChatHub.cs
@@ -8,5 +8,22 @@ namespace ChatCodeChallenge.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task PostCommandMessage(string user, string message)
+        {
+            if (message.StartsWith("/stock="))
+            {
+
+                var stockCode = message.Substring(7);
+
+                //this is goign to be the Stock API Call
+                //var stockInfo = await GetStockInfo(stockCode);
+
+
+                await Clients.All.SendAsync("ReceiveMessage", user, "message with command El magooooo");
+            }
+        }
+
+        
     }
 }
